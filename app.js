8891fb3656e71a6a24875b5779919fc5216f01ec
sharathxdev <sharath.y4512@gmail.com>
2025-06-28 21:36:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js CI/CD Pipeline    ');
+  res.send('Hello from Node.js CI/CD Pipeline     ');
 });
 
 app.listen(port, () => {
